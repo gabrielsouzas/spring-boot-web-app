5be070439d4255b9d2bae76b3f9acafda1c836d9
Gabriel Souza <104937852+gabrielsouzas@users.noreply.github.com>
2023-01-25 15:06:27 -0300

    Estrutura inicial

---
+++ src/main/java/com/webapplication/controllers/IndexController.java
@@ -0,0 +1,13 @@
+package com.webapplication.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+    
+    @RequestMapping("/a")
+    public String index(){
+        return "index";
+    }
+}
---
+++ src/main/java/com/webapplication/models/Evento.java
@@ -0,0 +1,41 @@
+package com.webapplication.models;
+
+public class Evento {
+    
+    private String nome;
+    private String local;
+    private String data;
+    private String horario;
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getLocal() {
+        return this.local;
+    }
+
+    public void setLocal(String local) {
+        this.local = local;
+    }
+
+    public String getData() {
+        return this.data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    public String getHorario() {
+        return this.horario;
+    }
+
+    public void setHorario(String horario) {
+        this.horario = horario;
+    }
+}
