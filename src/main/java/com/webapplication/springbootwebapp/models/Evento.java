6037156f23f6c96fe067bea340a828dfd01fe7b5
Gabriel Souza <104937852+gabrielsouzas@users.noreply.github.com>
2023-01-26 14:51:42 -0300

    Tabela de busca no banco

--- src/main/java/com/webapplication/springbootwebapp/models/Evento.java
+++ src/main/java/com/webapplication/springbootwebapp/models/Evento.java
@@ -10,7 +10,7 @@ import jakarta.persistence.Id;
 @Entity
 public class Evento implements Serializable {
     
-    private static final long serialversionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
